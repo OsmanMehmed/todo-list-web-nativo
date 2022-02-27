a0269b5a61912877807a5d4f4b032755e717df56
Osman Mehmed <osman@devanddel.com>
2022-02-27 12:35:36 +0100

    .

---
+++ funcionalidad.ts
@@ -0,0 +1,11 @@
+class tarea {
+
+    public nombre: string;
+    public estado: number;
+
+
+    constructor(nombre: string, estado: number){
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+}
