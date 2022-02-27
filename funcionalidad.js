f6a9b159406cd0cf54307334b94c4e15fe317e6c
Osman Mehmed <osman@devanddel.com>
2022-02-27 12:43:03 +0100

    cambiado version del código fuente a javascript

---
+++ funcionalidad.js
@@ -0,0 +1,14 @@
+
+console.log("Esto es un mensaje de prueba");
+
+class tarea {
+
+    nombre;
+    estado;
+
+
+    constructor(nombre, estado){
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+}
